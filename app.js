952cf268fc4b23d128c3e0fcc454ffd41558eac6
Kauã Xavier <kauaxavier032@gmail.com>
2024-04-16 18:44:28 -0300

    "Connect to database, add routes, and set up port"
    This commit connects to a database by importing and calling the connectDataBase function from a new file "./src/database/db". It also adds a router by import

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
-const app = express();
-app.use(cors);
+import connectDataBase from "./src/database/db";
+import router from "./src/routes/index.js";
 
 const port = 2000;
+
+const app = express();
+connectDataBase();
+
+app.use(express.json());
+app.use(cors);
+app.use(router);
+
 
 app.listen(port, () => {
   console.log(`🟢 Server On ${port}`);
