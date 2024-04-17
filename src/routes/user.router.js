13f8da4b250082d14e59d919228a0a8db2575fac
Kauã Xavier <kauaxavier032@gmail.com>
2024-04-17 17:42:38 -0300

    feat: "Add login route for users in user.router.js"
    This commit adds a new route for user login in the user router file. The route is mapped to the `loginController` in the `auth.controller.js` file

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/user.controller.js";
+import { loginController } from "../controllers/auth.controller.js";
 
 const userRouter = Router();
 
 userRouter.post("/register", CreateUserController);
+userRouter.get("/login", loginController);
 
 export default userRouter;
