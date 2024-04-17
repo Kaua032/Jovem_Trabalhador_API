e8847d6c506acbe48dbf3892a424e8945ee075ea
Kauã Xavier <kauaxavier032@gmail.com>
2024-04-17 15:38:45 -0300

    feat: model College add

---
+++ src/models/College.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const CollegeSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    city: {
+        type: String,
+        require: true,
+    }
+})
+
+const College = mongoose.model("College", CollegeSchema);
+
+export default College;
