1bfd9c3cef6b07d4f8f281c9708a2a2e71aa09a7
Kauã Xavier <kauaxavier032@gmail.com>
2024-04-19 18:28:43 -0300

    feat: Add authMiddleware to student routes: register and export
    This commit adds the authMiddleware to the student routes for registering and exporting students. Now, only authenticated users can access these endpoints.

--- src/routes/student.router.js
+++ src/routes/student.router.js
@@ -3,10 +3,11 @@ import {
   CreateStudentController,
   ExportStudentsController,
 } from "../controllers/student.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const studentRouter = Router();
 
-studentRouter.post("/register", CreateStudentController);
-studentRouter.get("/export", ExportStudentsController);
+studentRouter.post("/register", authMiddleware, CreateStudentController);
+studentRouter.get("/export", authMiddleware, ExportStudentsController);
 
 export default studentRouter;
