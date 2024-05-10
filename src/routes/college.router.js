6eca43af8cfd483bcb941e4d6894dbcb2af9b746
Kauã Xavier <kauaxavier032@gmail.com>
2024-05-10 13:57:37 -0300

    "Add delete college controller and route; add authMiddleware to delete student route"
    This diff adds a new controller function, `DeleteCollegeController`, and its corresponding route in `college.router.js`. It also updates `student

--- src/routes/college.router.js
+++ src/routes/college.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   CreateCollegeController,
+  DeleteCollegeController,
   GetAllCollegesController,
 } from "../controllers/college.controller.js";
 
@@ -8,5 +9,6 @@ const collegeRouter = Router();
 
 collegeRouter.post("/register", CreateCollegeController);
 collegeRouter.get("/all", GetAllCollegesController);
+collegeRouter.delete("/delete", DeleteCollegeController);
 
 export default collegeRouter;
