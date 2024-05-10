33c41db1fc189f8c20d5430c1a6f2540e3954107
Kauã Xavier <kauaxavier032@gmail.com>
2024-05-10 14:13:25 -0300

    "Remove DeleteCourseController and route from course.router"
    Explanation:
    The code diff shows that the `DeleteCourseController` function and its corresponding route have been removed from the `course.controller.js` and

--- src/routes/course.router.js
+++ src/routes/course.router.js
@@ -9,6 +9,5 @@ const courseRouter = Router();
 
 courseRouter.post("/register", CreateCourseController);
 courseRouter.get("/all", GetAllCoursesController);
-courseRouter.delete("/delete", DeleteCourseController);
 
 export default courseRouter;
