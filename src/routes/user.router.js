273466ad9ffdddd7bbedcaed8721e704b3e8e935
Kauã Xavier <kauaxavier032@gmail.com>
2024-05-14 14:47:25 -0300

    Remove axios and js-cookie dependencies Update package-lock.json and package.json Change GET /login to POST /login in user.router.js
    (Note: The commit message is written in the imperative tense

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,6 +5,6 @@ import { loginController } from "../controllers/auth.controller.js";
 const userRouter = Router();
 
 userRouter.post("/register", CreateUserController);
-userRouter.get("/login", loginController);
+userRouter.post("/login", loginController);
 
 export default userRouter;
